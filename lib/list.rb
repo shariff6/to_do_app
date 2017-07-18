dd071c163d20140ec484e5cc18cdf405223f2f65
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-18 09:48:59 +0300

    this commit will add a tasks methos to the list class

--- lib/list.rb
+++ lib/list.rb
@@ -35,4 +35,14 @@ class List
   end
   found_list
 end
+define_method(:tasks) do
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{self.id()};")
+    tasks.each() do |task|
+      description = task.fetch("description")
+      list_id = task.fetch("list_id").to_i()
+      list_tasks.push(Task.new(description,list_id))
+    end
+    list_tasks
+  end
 end
